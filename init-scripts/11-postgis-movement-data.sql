fa9e0834a089a2ee37ae379103faec4dbef6e36e
justin <justin2004@hotmail.com>
2025-02-18 17:03:51 -0600

    replicates the issue
    https://github.com/ontop/ontop/issues/839
    with query
    ```
    SELECT ?dist WHERE {
      ?s2 geo:asWKT_geog ?wkt .
      ?s1 geo:asWKT_geog ?wkt1 .
      bind(geof:distance(?wkt,?wkt1,uom:metre) as ?dist)
    }
    ```

--- init-scripts/11-postgis-movement-data.sql
+++ init-scripts/11-postgis-movement-data.sql
@@ -23,6 +23,6 @@ CREATE TABLE movement_report (
 );
 
 insert into movement_report(target_thing_id, geometry,geometry_string,geometry_json, dt, speed_mph) values (55, ST_GeomFromText('Point(-90 38.5)'),'Point(-90 38.5)','{"coordinates":[-90,38.5]}', '2004-10-19 10:23:54', 12) ;  
-insert into movement_report(target_thing_id, geometry,geometry_string, dt, speed_mph) values (55, ST_GeomFromText('Point(-90 38.501)'),'Point(-90 38.501)', '2004-10-19 11:23:54', 12) ;  
+insert into movement_report(target_thing_id, geometry,geometry_string,geometry_json, dt, speed_mph) values (55, ST_GeomFromText('Point(-90 38.501)'),'Point(-90 38.501)','{"coordinates":[-90,38.501]}', '2004-10-19 11:23:54', 12) ;  
 insert into movement_report(target_thing_id, geometry,geometry_string, dt, speed_mph) values (35, ST_GeomFromText('Point(-90.0 38.5002)'),'Point(-90.0 38.5002)', '2004-10-19 08:24:54', 12) ;  
 insert into movement_report(target_thing_id, geometry,geometry_string, dt, speed_mph) values (35, ST_GeomFromText('Point(-90.01 38.58)'),'Point(-90.01 38.58)', '2004-10-19 11:29:04', 129) ;  
